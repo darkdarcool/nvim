b2c1ad78e8676ed243ed5a7754dd9571d531d4da
darkdarcool <darkdarcool@gmail.com>
2024-02-26 20:10:22 -0500

    made code structure better

--- lua/lazy-setup.lua
+++ lua/lazy-setup.lua
@@ -13,4 +13,60 @@ vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>L", "<cmd>Lazy<CR>", { noremap = true, silent = true })
 
-require("lazy").setup("plugins")
+require("lazy").setup(
+  {
+    {
+      import = "plugins"
+    },
+    {
+      'windwp/nvim-autopairs',
+      event = "InsertEnter",
+      opts = {} -- this is equalent to setup({}) function
+    },
+    {
+      "nvim-telescope/telescope-file-browser.nvim",
+      dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
+    },
+    {
+      'numToStr/Comment.nvim',
+      opts = {
+          -- add any options here
+      },
+      lazy = false,
+    },
+    {
+      "folke/todo-comments.nvim",
+      dependencies = { "nvim-lua/plenary.nvim" },
+      opts = {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      }
+    },
+    {
+      "github/copilot.vim",
+      cmd = "Copilot",
+      event = "InsertEnter",
+      ft = "markdown"
+    },
+    {
+      "neovim/nvim-lspconfig",
+      config = function () 
+        -- show lsp documentation in a floating window
+        
+      end
+    },
+    {
+      'williamboman/mason.nvim',
+      config = function ()
+        require("mason").setup()
+      end
+    },
+    {
+      'williamboman/mason-lspconfig.nvim'
+    },
+    {
+      'xiyaowong/transparent.nvim',
+    }
+  }
+)
--- lua/plugins/lsp.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "neovim/nvim-lspconfig",
-  config = function () 
-    -- show lsp documentation in a floating window
-    
-  end
-}
